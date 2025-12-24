f981f974e0811cac1da2faab5c89f5bd11f8a0b2
alstj0127 <end020127@naver.com>
2025-12-24 16:01:53 +0900

    day1

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,5 +1,5 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+import logoDark from "./Velog-Icons_dark.svg";
+import logoLight from "./Velog-Icons_light.svg";
 
 export function Welcome() {
   return (
