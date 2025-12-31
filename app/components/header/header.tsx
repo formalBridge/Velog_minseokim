73c6395be38736f0fa2f5b4b97a7eef6136d788f
alstj0127 <end020127@naver.com>
2025-12-31 16:05:17 +0900

    메인 프로필섹션 ui 구현중(css미완성....)

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -3,40 +3,40 @@ import "./header.css";
 
 export function Header() {
   return (
-    <header className="header_block">
-      <div className="header_inner_block">
-        <div className="header_logo_block">
-          <Link className="header_logo" to="/">
+    <header className="Header_block">
+      <div className="Header_inner_block">
+        <div className="Header_logo_block">
+          <Link className="Header_logo" to="/">
             <img
               src="app/components/header/Velog-Icons_light.svg"
               title="Velog logo"
               alt="Velog logo"
             />
           </Link>
-          <Link className="header_user_name" to="/">
+          <Link className="Header_user_name" to="/">
             <span>Minoes.Log</span>
           </Link>
         </div>
       </div>
-      <div className="header_right">
-        <Link className="header_notification" to="/">
+      <div className="Header_right">
+        <Link className="Header_notification" to="/">
           <img
             src="app/components/header/notifications.svg"
             alt="notifications"
             title="notifications"
           />
         </Link>
-        <Link className="header_search" to="/">
+        <Link className="Header_search" to="/">
           <img
             src="app/components/header/search.svg"
             alt="search"
             title="search"
           />
         </Link>
-        <Link className="header_write" to="/">
+        <Link className="Header_write" to="/">
           <button className="round_button">새 글 작성</button>
         </Link>
-        <Link className="header_profile" to="/">
+        <Link className="Header_profile" to="/">
           <img
             src="app/components/header/profile.svg"
             alt="profile"
